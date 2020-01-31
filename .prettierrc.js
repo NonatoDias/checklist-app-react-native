c3afd3c102f0b19cb9e84cc50570fb58ebeaeb0c
Nonato Dias <nonato.developer@gmail.com>
2020-01-31 19:08:31 -0300

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 110,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	arrowParens: 'avoid',
+	endOfLine: 'lf',
+};
---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * @author Nonato Dias
+ *  Checklist app
+ *
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => (
+	<View style={styles.app}>
+		<Text>App test</Text>
+	</View>
+);
+
+const styles = StyleSheet.create({
+	app: {
+		backgroundColor: 'whitesmoke',
+	},
+});
+
+export default App;
